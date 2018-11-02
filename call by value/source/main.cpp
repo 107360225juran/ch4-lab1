cec32e80a29485e78f54e5984776baceb85f49a9
juran <juran11197@gmail.com>
2018-11-02 17:19:33 +0800

    123

---
+++ call by value/source/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int addbyone(int x);
+void main()
+{
+	int x = 100, y = addbyone(x);
+	printf("x=%d\n", x);
+	system("pause");
+
+}
+int addbyone(int x)
+{
+	(x)++;
+	printf("x=%d\n", x);
+	return x;
+}
